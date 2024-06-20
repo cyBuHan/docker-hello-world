2e6f3336f341a4aa8074c7e1d20949e8ec0a79e8
Han Bu <2968078611@qq.com>
2024-06-20 09:14:05 +0800

    a little change

---
+++ JScode/func.js
@@ -0,0 +1,7 @@
+function myFunction() {    
+    document.getElementById("demo").innerHTML = "OK! Let's design a website.";
+}
+
+function myFunction2() {
+    document.getElementById("demo2").innerHTML = "";
+}
